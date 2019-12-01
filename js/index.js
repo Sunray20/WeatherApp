3e09214e659aad84eacdbf1d4772b280becfc34d
Dávid Jakab <jakabdavid20@gmail.com>
2019-12-01 12:33:28 +0100

    Refactor

--- js/index.js
+++ js/index.js
@@ -1,10 +1,25 @@
-function httpGetAsync(theUrl, callback)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
-    }
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-    xmlHttp.send(null);
+let selectedLocation;
+
+document.getElementById("refresh-button").addEventListener("click", () => {
+    selectedLocation = document.getElementById("city-select").value;
+    fetchData()
+        .then((response) => {
+            let weatherResponse = response.data;
+            updateComponent(weatherResponse.name, weatherResponse.main.temp);
+        });
+});
+
+function fetchData() {
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${selectedLocation},hu&APPID=af09d52af847e74ed5d930cf561434fc`;
+    return axios.get(url);
+}
+
+function updateComponent(city, temp) {
+    document.getElementById("city-name").innerHTML = city;
+    document.getElementById("temp").innerHTML = getTempString(temp);
+}
+
+function getTempString(temp) {
+    temp -= 273;
+    return Number(temp.toFixed(2)) + " °C";
 }
